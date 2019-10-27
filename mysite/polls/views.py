c8964c15cf829c02448774ae736e3f34b8d9c933
Lucas Amilivia <amilivialucas4@gmail.com>
2019-10-27 19:52:49 -0300

    fin del paso 5, agregando pruebas sobre modelos y vistas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
+from django.utils import timezone
 from .models import Choice, Question
 
 
@@ -18,6 +18,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
