f50d16f6faa616121c9df8b075c2b034c52cf40a
Lucas Amilivia <amilivialucas4@gmail.com>
2019-10-27 01:28:13 -0300

    agrego namespace y parametrizo urls de las vistas

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
+# template's name can be used as a refenece  template as synonymous of urls
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/,
+    # the 'name' value as called by the {% url %} template tag
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
