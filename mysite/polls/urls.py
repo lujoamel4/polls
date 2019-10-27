311740591b069852d48dca96c9033b5baf231cf7
Lucas Amilivia <amilivialucas4@gmail.com>
2019-10-27 02:21:25 -0300

    fin del paso 4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,12 +7,12 @@ app_name = 'polls'
 # template's name can be used as a refenece  template as synonymous of urls
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/,
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5/
     # the 'name' value as called by the {% url %} template tag
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
